0988c98cd4319b5e6c13d3ec91d58d7822f98eae
Tamil Selvan <87596375+metamil@users.noreply.github.com>
2023-12-13 16:40:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+app.get("/",(req,res)=>{
+       res.send("hey");
+})
+
 app.get('/items/:vegetable', (req, res) => {
        let veg = req.params.vegetable;
        res.send(`This is the page for ${veg}`);
